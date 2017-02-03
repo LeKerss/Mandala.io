c69d2182cfab20d607bd3413c9e6964325e25fb2
LeKerss <annas9001@gmail.com>
2017-02-03 14:30:13 +0100

    Socket.io setup

--- client/sketch.js
+++ client/sketch.js
@@ -1,11 +1,25 @@
-function setup() {
+import io from 'socket.io-client'
+
+const SIZE_WIDTH = 50
+const SIZE_HEIGHT = 50
+
+let buffer
+let socket
+
+window.setup = () => {
+    buffer = []
+    socket = io.connect()
     createCanvas(640, 480)
+    fill(0)
 }
 
-function draw() {
-    fill(0)
+window.draw = () => {
     if(mouseIsPressed) {
-        ellipse(mouseX, mouseY, 50, 50)
+        ellipse(mouseX, mouseY, SIZE_HEIGHT, SIZE_WIDTH)
+    }
+    var pos = buffer.shift()
+    if(pos){
+        ellipse(pos.x, pos.y, SIZE_HEIGHT, SIZE_WIDTH)
     }
 
 }
