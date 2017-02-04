c047aac0e315193ac87d460fe3ea55bb0d50ea2f
LeKerss <annas9001@gmail.com>
2017-02-04 15:09:24 +0100

    Size selection
    The user can now choose the size of the ellipse he is drawing.
    Changes are effectively communicated to the server, and from the server
    back to the clients.

--- client/sketch.js
+++ client/sketch.js
@@ -1,19 +1,16 @@
 import io from 'socket.io-client'
 
-const   SIZE_WIDTH = 50,
-        SIZE_HEIGHT = 50
-
-let mColor = '#000000'
-let canvasHover = false;
-
-let buffer
-let socket = io.connect('http://127.0.0.1:3000')
+let     mWidth = 50,
+        mHeight = 50,
+        mColor = '#000000',
+        canvasHover = false,
+        buffer = [],
+        socket = io.connect('http://127.0.0.1:3000')
 
 window.setup = () => {
-    buffer = []
+    ellipseMode(CENTER)
     let canvas = createCanvas(640, 480)
     canvas.parent('sketch-holder')
-
 }
 
 window.draw = () => {
@@ -21,19 +18,22 @@ window.draw = () => {
         fill(mColor)
         let myX = mouseX,
             myY = mouseY;
-        ellipse(myX, myY, SIZE_WIDTH, SIZE_HEIGHT)
+        ellipse(myX, myY, mWidth, mHeight)
         socket.emit('draw_motif', {
             motif:{
                 color: mColor,
                 x: myX,
-                y: myY
+                y: myY,
+                width: mWidth,
+                height: mHeight
             }
         })
     }
     let motif = buffer.shift()
     if(motif){
+
         fill(motif.color)
-        ellipse(motif.x, motif.y, SIZE_HEIGHT, SIZE_WIDTH)
+        ellipse(motif.x, motif.y, motif.width, motif.height)
     }
 }
 
@@ -46,10 +46,15 @@ socket.on('clear_page', function(data) {
 })
 
 
-
 window.onload = function() {
     document.getElementById("motif-color").addEventListener("input", function () {
             mColor = this.value;
+    })
+    document.getElementById("motif-width").addEventListener("input", function () {
+            mWidth = parseInt(this.value);
+    })
+    document.getElementById("motif-height").addEventListener("input", function () {
+            mHeight = parseInt(this.value);
     })
     document.getElementById("clear-page").addEventListener("click", function () {
             socket.emit('clear_page', {clearer: ""})
