f13f180cfbcbe1f81a6c328af4f226829713074b
LeKerss <annas9001@gmail.com>
2017-02-07 11:53:46 +0100

    Closes #6 Stroke
    Possiblity to toggle stroke (border) and choose its color and size in
    px.

--- client/sketch.js
+++ client/sketch.js
@@ -1,12 +1,15 @@
 import io from 'socket.io-client'
 
-let     mShape = "ellipse",
-        mWidth = 50,
-        mHeight = 50,
-        mColor = '#000000',
-        canvasHover = false,
-        buffer = [],
-        socket = io.connect('http://127.0.0.1:3000')
+let mShape = "ellipse",
+    mWidth = 50,
+    mHeight = 50,
+    mColor = '#000000',
+    canvasHover = false,
+    buffer = [],
+    mStroke = true,
+    mStrokeColor = '#000000',
+    mStrokeWeight = 1,
+    socket = io.connect('http://127.0.0.1:3000')
 
 window.setup = () => {
     ellipseMode(CENTER)
@@ -15,7 +18,7 @@ window.setup = () => {
 }
 
 window.draw = () => {
-    if(canvasHover && mouseIsPressed) {
+    if (canvasHover && mouseIsPressed) {
         let myX = mouseX,
             myY = mouseY;
         let motif = {
@@ -24,36 +27,49 @@ window.draw = () => {
             x: myX,
             y: myY,
             width: mWidth,
-            height: mHeight
+            height: mHeight,
+            stroke: mStroke,
+            strokeColor: mStrokeColor,
+            strokeWeight: mStrokeWeight
         }
 
         setMotif(motif)
 
-        socket.emit('draw_motif', { motif })
+        socket.emit('draw_motif', {
+            motif
+        })
     }
 
     let motif = buffer.shift()
-    if(motif){
+    if (motif) {
         setMotif(motif)
     }
 }
 
 window.setMotif = (motif) => {
+    if (motif.stroke) {
+        stroke(motif.strokeColor)
+        strokeWeight(motif.strokeWeight)
+    } else {
+        noStroke()
+    }
+
     fill(motif.color)
+
     switch (motif.shape) {
         case "ellipse":
             ellipse(motif.x, motif.y, motif.width, motif.height)
-        break;
+            break;
         case "rect":
             rect(motif.x, motif.y, motif.width, motif.height)
-        break;
+            break;
         default:
             rect(motif.x, motif.y, motif.width, motif.height)
     }
     fill(mColor)
 }
 
-socket.on('draw_motif', function(data){
+socket.on('draw_motif', function(data) {
     buffer.push(data.motif)
 })
 
@@ -63,23 +79,37 @@ socket.on('clear_page', function(data) {
 
 
 window.onload = function() {
-    document.getElementById("motif-color").addEventListener("input", function () {
-            mColor = this.value;
+    document.getElementById("motif-color").addEventListener("input", function() {
+        mColor = this.value
     })
-    document.getElementById("motif-shape-ellipse").addEventListener("click", function () {
-            mShape = this.value
+    document.getElementById("motif-shape-ellipse").addEventListener("click", function() {
+        mShape = this.value
     })
-    document.getElementById("motif-shape-rectangle").addEventListener("click", function () {
-            mShape = this.value
+    document.getElementById("motif-shape-rectangle").addEventListener("click", function() {
+        mShape = this.value
     })
-    document.getElementById("motif-width").addEventListener("input", function () {
-            mWidth = parseInt(this.value);
+    document.getElementById("motif-width").addEventListener("input", function() {
+        mWidth = parseInt(this.value);
     })
-    document.getElementById("motif-height").addEventListener("input", function () {
-            mHeight = parseInt(this.value);
+    document.getElementById("motif-height").addEventListener("input", function() {
+        mHeight = parseInt(this.value);
     })
-    document.getElementById("clear-page").addEventListener("click", function () {
-            socket.emit('clear_page', {clearer: ""})
+    document.getElementById("clear-page").addEventListener("click", function() {
+        socket.emit('clear_page', {
+            clearer: ""
+        })
+    })
+    document.getElementById("motif-stroke").addEventListener("click", function() {
+        mStroke = true
+    })
+    document.getElementById("motif-stroke-color").addEventListener("input", function() {
+        mStrokeColor = this.value
+    })
+    document.getElementById("motif-stroke-weight").addEventListener("input", function() {
+        mStrokeWeight = this.value
+    })
+    document.getElementById("motif-no-stroke").addEventListener("click", function() {
+        mStroke = false
     })
     window.setHover = (state) => {
         canvasHover = state
