f3ebbac6ce5a6560b4efc3d6403bd0fd433c52d5
Le Kerss <annas9001@gmail.com>
2017-11-15 10:37:24 -0800

    persistence with redis

--- server/server.js
+++ server/server.js
@@ -1,26 +1,39 @@
 var express = require('express'),
     app = express(),
+    redis = require('redis'),
     http = require('http'),
     socketIo = require('socket.io')
 
 var server = http.createServer(app)
 var io = socketIo.listen(server)
+var redisClient = redis.createClient('6379', '127.0.0.1')
+
 io.set('origins', '*:*')
 server.listen(3000)
 app.use(express.static(__dirname + '/public'))
 console.log("Server running on 170.0.0.1:3000")
 
-var motif_history = []
-
 io.on('connection', function(socket) {
-    socket.emit('init_page', { page : motif_history })
+    redisClient.lrange('motiflist', 0, -1, function(err, reply){
+        socket.emit('init_page', { 
+            page : reply 
+        })
+    })
     
     socket.on('draw_motif', function(data) {
-        motif_history.push(data.motif)
+        var intKey = redisClient.incr("motifId")
+        var motifKey = 'motif' + intKey
+
+        redisClient.hmset(motifKey, data.motif)
+        redisClient.rpush(['motiflist', motifKey])
+
         io.emit('draw_motif', { motif: data.motif })
     })
     socket.on('clear_page', function(data){
-        motif_history = [];
+        redisClient.lrange('motiflist', 0, -1, function(err, reply){
+            redisClient.hdel(reply)
+            redisClient.ltrim('motiflist', 0, 0)
+        })
         io.emit('clear_page', {clearer: data.clearer})
     })
 })
